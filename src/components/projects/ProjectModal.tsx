ba06182f94871d90d5328832dc9458669992f406
ynuevo40-cpu <heider.gonzalez@unireformada.edu.co>
2026-03-26 00:01:37 -0500

    Fix ProjectModal ReferenceError: add missing DialogDescription import
    - Add DialogDescription to dialog imports to fix undefined variable error
    - Resolves ReferenceError on line 678 when using DialogDescription component
    - Project modal should now open without crashing

--- src/components/projects/ProjectModal.tsx
+++ src/components/projects/ProjectModal.tsx
@@ -5,6 +5,7 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
 } from '@/components/ui/dialog';
 import { MediaRenderer } from "@/components/media/MediaRenderer";
 
