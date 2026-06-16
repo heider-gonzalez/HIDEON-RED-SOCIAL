a33edfad37f74a4e6bd77f005cbcbfdd0920894f
ynuevo40-cpu <heider.gonzalez@unireformada.edu.co>
2026-06-16 14:27:07 -0500

    Remove QuickPostBox - keep only FacebookPostCreator as the single post creation component

--- src/components/feed/index.ts
+++ src/components/feed/index.ts
@@ -2,5 +2,4 @@ export { Feed } from "./Feed";
 export { FeedContent } from "./FeedContent";
 export { FeedSkeleton } from "./FeedSkeleton";
 export { EmptyFeed } from "./EmptyFeed";
-export { QuickPostBox } from "./QuickPostBox";
 export { PublicFeedWall } from "./PublicFeedWall";
